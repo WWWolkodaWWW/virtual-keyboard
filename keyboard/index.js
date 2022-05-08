3fb386c7f511325b695b2ba58095b2dab48d6950
WWWolkodaWWW <80260640+WWWolkodaWWW@users.noreply.github.com>
2022-05-09 00:33:36 +0200

    refactor: delete empty lines, add self-check

--- keyboard/index.js
+++ keyboard/index.js
@@ -1,3 +1,5 @@
+
+
 const keyboard = {
 	elements: {
 		wrapper: null,
@@ -79,7 +81,7 @@ const keyboard = {
 			btnElement.classList.add('btn');
 			btnElement.isLetterButton = false;
 
-			switch (key) { 
+			switch (key) {
 				case "Backspace":
 					btnElement.classList.add('medium');
 					btnElement.dataset.name = key;
@@ -89,7 +91,6 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					break;
-				
 				case "Delete":
 					btnElement.innerText = "Del";
 					btnElement.dataset.name = key;
@@ -98,7 +99,6 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					break;
-				
 				case "Enter":
 					btnElement.classList.add('medium');
 					btnElement.innerText = "Enter";
@@ -108,28 +108,24 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					break;
-				
 				case "Tab":
 					btnElement.classList.add('medium');
 					btnElement.dataset.name = key;
 					btnElement.innerText = "Tab";
 					btnElement.addEventListener('click', () => {
 						this.properties.value += "\t";
-						// this.properties.value += "    ";
 						this.fireEvent("oninput");
 					});
 					break;
-				
 				case "Space":
 					btnElement.classList.add('large');
 					btnElement.dataset.name = key;
-					
 					btnElement.addEventListener('click', () => {
-						this.properties.value += " ";
+						let cursorStart = document.querySelector('.keyboard-screen').selectionStart;
+						this.properties.value = this.properties.value.slice(0, cursorStart) + " " + this.properties.value.slice(cursorStart, this.properties.value.length);
 						this.fireEvent("oninput");
 					});
 					break;
-				
 				case "CapsLock":
 					btnElement.classList.add('medium', 'on');
 					btnElement.dataset.name = key;
@@ -139,7 +135,6 @@ const keyboard = {
 						btnElement.classList.toggle('active', this.properties.capsLock);
 					});
 					break;
-				
 				case "ShiftRight":
 					btnElement.classList.add('medium');
 					btnElement.dataset.name = key;
@@ -148,7 +143,6 @@ const keyboard = {
 						
 					});
 					break;
-				
 				case "ShiftLeft":
 					btnElement.classList.add('medium');
 					btnElement.dataset.name = key;
@@ -156,9 +150,7 @@ const keyboard = {
 					btnElement.addEventListener('click', () => {
 						
 					});
-
 					break;
-				
 				case "ControlRight":
 					btnElement.textContent = "Ctrl";
 					btnElement.dataset.name = key;
@@ -166,7 +158,6 @@ const keyboard = {
 						
 					});
 					break;
-				
 				case "ControlLeft":
 					btnElement.innerText = "Ctrl";
 					btnElement.dataset.name = key;
@@ -174,7 +165,6 @@ const keyboard = {
 						
 					});
 					break;
-				
 				case "AltRight":
 					btnElement.innerText = "Alt";
 					btnElement.dataset.name = key;
@@ -182,7 +172,6 @@ const keyboard = {
 						
 					});
 					break;
-				
 				case "AltLeft":
 					btnElement.innerText = "Alt";
 					btnElement.dataset.name = key;
@@ -190,51 +179,47 @@ const keyboard = {
 						
 					});
 					break;
-				
 				case "Win":
 					btnElement.innerText = "Win";
-					btnElement.dataset.name = key;
+					btnElement.dataset.name = "MetaLeft";
 					btnElement.addEventListener('click', () => {
 						
 					});
 					break;
-				
 				case "ArrowUp":
 					btnElement.classList.add('arrow');
 					btnElement.innerHTML = "&uArr;";
-
+					btnElement.dataset.name = "ArrowUp";
 					btnElement.addEventListener('click', () => {
 						
 					});
 					break;
-				
 				case "ArrowDown":
 					btnElement.classList.add('arrow');
 					btnElement.innerHTML = "&dArr;";
-
+					btnElement.dataset.name = "ArrowDown";
 					btnElement.addEventListener('click', () => {
 						
 					});
 					break;
-			
 				case "ArrowLeft":
 					btnElement.classList.add('arrow');
 					btnElement.innerHTML = "&lArr;";
-
+					btnElement.dataset.name = "ArrowLeft";
 					btnElement.addEventListener('click', () => {
-						
+						let cursorStart = document.querySelector('.keyboard-screen').selectionStart;
+						this.properties.value = this.properties.value.slice(0, cursorStart) + this.properties.value.slice(cursorStart, this.properties.value.length);
+						this.fireEvent("oninput");
 					});
 					break;
-			
 				case "ArrowRight":
 					btnElement.classList.add('arrow');
 					btnElement.innerHTML = "&rArr;";
-					
+					btnElement.dataset.name = "ArrowRight";
 					btnElement.addEventListener('click', () => {
 						
 					});
 					break;
-				
 				case "`":
 					btnElement.dataset.name = "Backquote";
 					btnElement.innerText = "`";
@@ -244,7 +229,6 @@ const keyboard = {
 					});
 					btnElement.isLetterButton = true;
 					break;
-				
 				case "1":
 					btnElement.dataset.name = "Digit1";
 					btnElement.innerText = "1";
@@ -254,7 +238,6 @@ const keyboard = {
 					});
 					btnElement.isLetterButton = true;
 					break;
-				
 				case "2":
 					btnElement.dataset.name = "Digit2";
 					btnElement.innerText = "2";
@@ -263,7 +246,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "3":
 					btnElement.dataset.name = "Digit3";
 					btnElement.innerText = "3";
@@ -272,7 +255,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "4":
 					btnElement.dataset.name = "Digit4";
 					btnElement.innerText = "4";
@@ -281,7 +264,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "5":
 					btnElement.dataset.name = "Digit5";
 					btnElement.innerText = "5";
@@ -290,7 +273,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "6":
 					btnElement.dataset.name = "Digit6";
 					btnElement.innerText = "6";
@@ -299,7 +282,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "7":
 					btnElement.dataset.name = "Digit7";
 					btnElement.innerText = "7";
@@ -308,7 +291,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "8":
 					btnElement.dataset.name = "Digit8";
 					btnElement.innerText = "8";
@@ -317,7 +300,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "9":
 					btnElement.dataset.name = "Digit9";
 					btnElement.innerText = "9";
@@ -326,7 +309,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "0":
 					btnElement.dataset.name = "Digit0";
 					btnElement.innerText = "0";
@@ -335,7 +318,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "-":
 					btnElement.dataset.name = "Minus";
 					btnElement.innerText = "-";
@@ -344,7 +327,7 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-				break;
+					break;
 				case "=":
 					btnElement.dataset.name = "Equal";
 					btnElement.innerText = "=";
@@ -354,7 +337,78 @@ const keyboard = {
 					});
 					btnElement.isLetterButton = true;
 					break;
-				
+				case "\\":
+					btnElement.dataset.name = "Backslash";
+					btnElement.innerText = "\\";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case "[":
+					btnElement.dataset.name = "BracketLeft";
+					btnElement.innerText = "[";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case "]":
+					btnElement.dataset.name = "BracketRight";
+					btnElement.innerText = "[";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case ";":
+					btnElement.dataset.name = "Semicolon";
+					btnElement.innerText = ";";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case "'":
+					btnElement.dataset.name = "Quote";
+					btnElement.innerText = "'";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case "?":
+					btnElement.dataset.name = "Slash";
+					btnElement.innerText = "?";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case ",":
+					btnElement.dataset.name = "Comma";
+					btnElement.innerText = ",";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
+				case ".":
+					btnElement.dataset.name = "Period";
+					btnElement.innerText = ".";
+					btnElement.addEventListener('click', () => {
+						this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+						this.fireEvent("oninput");
+					});
+					btnElement.isLetterButton = true;
+					break;
 				default:
 					btnElement.textContent = key.toLowerCase();
 					
@@ -363,39 +417,39 @@ const keyboard = {
 						this.fireEvent("oninput");
 					});
 					btnElement.isLetterButton = true;
-					break;				
+					break;
 			}
 
 			fragment.appendChild(btnElement);
 
-			if (insertLIneBreak) { 
+			if (insertLIneBreak) {
 				fragment.appendChild(document.createElement('br'));
 			}
-		});	
+		});
 		
 		return fragment;
 	},
 
-	fireEvent(name) { 
-		if (typeof this.properties[name] == "function") { 
+	fireEvent(name) {
+		if (typeof this.properties[name] == "function") {
 			this.properties[name](this.properties.value);
 		}
 	},
 
 	toggleCapsLock() {
 		this.properties.capsLock = !this.properties.capsLock;
-		for (const key of this.elements.btns) { 
-			if (key.isLetterButton) { 
+		for (const key of this.elements.btns) {
+			if (key.isLetterButton) {
 				key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
 			}
 		}
 	},
 
-	works(initialValue, oninput) { 
+	works(initialValue, oninput) {
 		this.properties.value = initialValue || "";
 		this.properties.oninput = oninput;
 	}
-}
+};
 
 document.onkeydown = function (event) { 
 
@@ -405,12 +459,10 @@ document.onkeydown = function (event) {
 		if (isCapsLockOn) { 
 			button.classList.add('active');
 		}
-
 	} else { 
 		button.classList.add('active');
 	}
 	event.preventDefault();	
-	let selectionStart = document.querySelector('.keyboard-screen').selectionStart;
 	button.click();
 }
 
@@ -422,8 +474,6 @@ document.onkeyup = function (event) {
 		if (!isCapsLockOn) { 
 			button.classList.remove('active');
 		}
-			
-
 	} else { 
 		button.classList.remove('active');
 	}
@@ -433,3 +483,24 @@ document.onkeyup = function (event) {
 window.addEventListener('DOMContentLoaded', function () {
 	keyboard.init();
 });
+
+
+alert(`привет :) упрощу проверку:
+- отсутствуют classes, property destructuring и ESLint
+- накрутить переключения языка не успела
+- пенальти тоже нет, ибо как может ругатся то, чего нет xDD
+
+Плюсы в карму за расширенный отзыв, ибо дается джава скрипт нелегко, посему буду рада помощи.
+`);
+
+console.log(`самопроверка: 
+1. (20) the generation of DOM elements is implemented. body in the index.html is empty (can contain only script tag)
+2. (10) pressing a key on a physical keyboard highlights the key on the virtual keyboard (you should check keystrokes of numbers, letters, punctuation marks, backspace, del (if it's present), enter, shift, alt, ctrl, tab, caps lock, space, arrow keys
+3. ( - ) switching keyboard layouts between English and another language is implemented. Selected language should be saved and used on page reload. A keyboard shortcut for switching a language should be specified on the page
+4. (15) mouse clicks on buttons of the virtual keyboard or pressing buttons on a physical keyboard inputs characters to the input field (text area)
+5. (15) animation of pressing a key is implemented
+6. ( - ) usage of ES6+ features (classes, property destructuring, etc
+7. ( - ) usage of ESLint
+8. (10) requirements to the repository, commits and pull request are met
+9. ( - ) there're errors related to the executable code (errors like favicon.ico: Failed to load resource: the server responded with a status of 404 are not taken into account) or there're eslint-config-airbnb-base warnings
+`);
